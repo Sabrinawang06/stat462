ebdad0a0687c6dbec3d9530880ea8d2385052c44
Sabrina <sabrinawang06@gmai.com>
2018-11-20 22:25:57 -0500

    logistic

--- final_report.R
+++ final_report.R
@@ -3,6 +3,26 @@
 #read in the data
 wine<-read.csv("wine_sample.csv", header=TRUE)
 summary(wine[,-1])
+attach(wine)
+
+###check vif
+library(xtable)
+cor<-round(cor(wine[-c(1,13,14)]),2)
+upper<-cor
+upper[upper.tri(cor)]<-""
+upper<-as.data.frame(upper)
+table<-xtable(upper)
+print(table, typle="html",comment=FALSE,scalebox = 0.7)
+
+#density has -0.7 with alcohol
+library(car)
+plot(density~alcohol)
+vif(lm(quality~.-X-quality, data=wine)) #remove density
+vif(lm(quality~.-X-density-quality, data=wine))
+
+
+#####Regular Regression model
+
 
 #####Logistic model#####
 summary(wine$quality)
@@ -12,11 +32,12 @@ quality_logi<-vector('numeric',1000)
 quality_logi[wine$quality<=5]=0
 quality_logi[wine$quality>5]=1
 
-logistic_fit=glm(quality_logi~.-X-quality,data=wine,family=binomial(link="logit"))
+logistic_fit=glm(quality_logi~.-X-density-quality,data=wine,family=binomial(link="logit"))
 summary(logistic_fit)
 par(mfrow=c(2,2))
 plot(logistic_fit)
 
+####alpha-to-remove method
 reduced_model<-update(logistic_fit, .~.-citric.acid)
 summary(reduced_model)
 reduced_model2<-update(reduced_model, .~.-total.sulfur.dioxide)
@@ -27,3 +48,20 @@ reduced_model4<-update(reduced_model3, .~.-fixed.acidity)
 summary(reduced_model4)
 reduced_model5<-update(reduced_model4, .~.-free.sulfur.dioxide)
 summary(reduced_model5)
+
+####adjuested r square
+require(bestglm)
+Xy<-cbind(wine[,-c(1,9,13)],quality_logi)
+bestglm(Xy,IC="AIC")
+
+
+#####Ordinal lOgsitic
+require(foreign)
+require(ggplot2)
+require(MASS)
+require(Hmisc)
+require(reshape2)
+quality_factor<-as.factor(wine$quality)
+ordinal_fit<-polr(quality_factor~.-X-density-quality,data=wine, Hess=TRUE)
+summary(ordinal_fit)
+
